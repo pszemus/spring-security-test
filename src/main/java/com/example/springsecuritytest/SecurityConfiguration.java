fb9b40194747a3b45678183276b81c582cb004a3
Przemysław Sobala <przemyslaw.sobala@grupawp.pl>
2022-06-30 08:28:21 +0200

    Migrate from WebSecurityConfigurerAdapter to SecurityFilterChain

--- src/main/java/com/example/springsecuritytest/SecurityConfiguration.java
+++ src/main/java/com/example/springsecuritytest/SecurityConfiguration.java
@@ -1,30 +1,34 @@
 package com.example.springsecuritytest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class SecurityConfiguration {
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication()
-				.passwordEncoder(encoder)
-				.withUser("spring")
-				.password(encoder.encode("secret"))
-				.roles("USER");
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(authorize -> authorize
+						.mvcMatchers("/private/**").authenticated()
+						.mvcMatchers("/public/**").permitAll())
+				.httpBasic(withDefaults())
+				.build();
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/private/**").authenticated()
-				.antMatchers("/public/**").permitAll()
-				.and().httpBasic();
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("spring")
+				.password("secret")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
 	}
 }
